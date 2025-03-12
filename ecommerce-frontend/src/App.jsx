be99bb660ea56f666d1e4c3f92fceb9c8c1fba88
saravananCodeExplorer <evergoldhariny2007@gmail.com>
2025-03-12 18:09:41 +0530

    admin added product

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.jsx";
+import Home from "./components/Home/Home.jsx"; 
 import ProductList from "./components/ProductList/ProductList";
 import Cart from "./components/Cart/Cart.jsx"; 
 import Admin from "./components/Admin/Admin.jsx"
 import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
+// import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
 import Footer from "./components/Footer/Footer";
@@ -14,9 +15,10 @@ function App() {
     <CartProvider>
       <Router>
         <Header />
-        <Hero />
+        {/* <Hero /> */}
         <Routes>
-          <Route path="/" element={<ProductList />} />
+        <Route path="/" element={<Home />} />
+          <Route path="/productlist" element={<ProductList />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/cart" element={<Cart />} />
